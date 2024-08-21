1a4cbeae1a298e8fae537785e4eabe59428131d9
cgp-client <code.gen.pro.system@gmail.com>
2024-08-21 09:20:20 +0300

    Adding src/data/notes.js

---
+++ src/data/notes.js
@@ -0,0 +1,22 @@
+export const notes = [
+  {
+    id: 1,
+    title: "Note 1",
+    content: "This is the content of Note 1",
+    status: "active"
+  },
+  {
+    id: 2,
+    title: "Note 2",
+    content: "This is the content of Note 2",
+    status: "active"
+  },
+  {
+    id: 3,
+    title: "Note 3",
+    content: "This is the content of Note 3",
+    status: "active"
+  }
+];
+
+export default notes;
